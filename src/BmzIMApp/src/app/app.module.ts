663db401f40d3d38b6f5c2f67f3f3a2d686ff908
BMZ <blademasterzhang@126.com>
2017-06-13 17:46:28 +0800

    add page

--- src/BmzIMApp/src/app/app.module.ts
+++ src/BmzIMApp/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
+import { SlidesPage } from '../pages/slides/slides';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ModalContentPage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,8 +18,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
+    SlidesPage,
     ContactPage,
     HomePage,
+    ModalContentPage,
     TabsPage
   ],
   imports: [
@@ -27,8 +32,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
+    SlidesPage,
     ContactPage,
     HomePage,
+    ModalContentPage,
     TabsPage
   ],
   providers: [
